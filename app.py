521c090f2e329b066d814acff519539d2f190f91
arie5981 <arie5981@gmail.com>
2026-06-10 11:45:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # מציג את קובץ ה-index.html מתוך תיקיית templates
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # קריטי עבור Fly.io - הקשבה לפורט 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
